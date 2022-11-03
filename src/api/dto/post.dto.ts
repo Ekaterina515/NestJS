1fa649145db30fbdc1d30b58c0ba8b314dfda71f
EkaterinaF <9150221@mail.ru>
2022-11-03 15:55:24 +0200

    DZ2

---
+++ src/api/dto/post.dto.ts
@@ -0,0 +1,10 @@
+import { Comment } from './comment.dto';
+export class Posts {
+  id!: number;
+  name!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+  description!: string;
+  text!: string;
+  comments!: Comment[];
+}
